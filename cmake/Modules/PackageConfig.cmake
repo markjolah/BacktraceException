e14fbc33ab5a65573c2856f13f7ea14d29f73552
Mark J. Olah <markjolah@users.noreply.github.com>
2017-11-13 23:56:14 -0700

    PackageConfig and Build improvements

--- cmake/Modules/PackageConfig.cmake
+++ cmake/Modules/PackageConfig.cmake
@@ -1,4 +1,4 @@
-# BacktraceException-PackageConfig.cmake
+# PackageConfig.cmake
 #
 # Mark J. Olah (mjo@cs.unm DOT edu)
 # Copyright 2017
@@ -15,7 +15,6 @@ set(VERSION_CONFIG_FILE ${CONFIG_DIR}/${PROJECT_NAME}ConfigVersion.cmake)
 set(CONFIG_FILE ${CONFIG_DIR}/${CONFIG_NAME}.cmake)
 set(CONFIG_TEMPLATE_FILE ${CMAKE_SOURCE_DIR}/cmake/Templates/${CONFIG_NAME}.cmake.in)
 set(CONFIG_INSTALL_DIR lib/cmake/${PROJECT_NAME}) #Where to install project Config.cmake and ConfigVersion.cmake files
-set(SHARED_CMAKE_INSTALL_DIR share/${PROJECT_NAME}/cmake) #Where to install shared .cmake build scripts for downstream
 
 ## Generate Package Config files for downstream projects to utilize
 #Generate: BacktraceExceptionConfigVersion.cmake
@@ -24,7 +23,7 @@ write_basic_package_version_file(${VERSION_CONFIG_FILE} COMPATIBILITY SameMajorV
 #Generate: BacktraceExceptionConfig.cmake
 configure_package_config_file(${CONFIG_TEMPLATE_FILE} ${CONFIG_FILE} 
                               INSTALL_DESTINATION ${CONFIG_INSTALL_DIR}
-                              PATH_VARS CONFIG_INSTALL_DIR SHARED_CMAKE_INSTALL_DIR)
+                              PATH_VARS CONFIG_INSTALL_DIR)
 
 #Install package config files
 install(FILES ${CONFIG_FILE} ${VERSION_CONFIG_FILE} 
