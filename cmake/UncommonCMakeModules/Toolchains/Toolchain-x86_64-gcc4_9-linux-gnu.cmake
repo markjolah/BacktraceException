e16b6473bff23436c9b23cbc924b9ecdcc93d2cd
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-06 13:54:01 -0700

    git subrepo pull cmake/UncommonCMakeModules
    subrepo:
      subdir:   "cmake/UncommonCMakeModules"
      merged:   "3796f4f"
    upstream:
      origin:   "https://github.com/markjolah/UncommonCMakeModules.git"
      branch:   "master"
      commit:   "3796f4f"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

---
+++ cmake/UncommonCMakeModules/Toolchains/Toolchain-x86_64-gcc4_9-linux-gnu.cmake
@@ -0,0 +1,19 @@
+# MexIFace CMake build system
+# Mark J. Olah (mjo@cs.unm DOT edu)
+# 01-2018
+#
+# Toolchain-x86_64-gcc4_9-linux-gnu.cmake
+#
+# Toolchain for cross-compiling to a linux matlab9_3 (and earlier)
+# environment using gcc-4.9.4.
+#
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CXX_COMPILER g++-4.9.4)
+set(CMAKE_FIND_ROOT_PATH $ENV{X86_64_GCC4_9_LINUX_GNU_ROOT})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY True)
+set(CMAKE_EXPORT_NO_PACKAGE_REGISTRY True)
---
+++ cmake/UncommonCMakeModules/Toolchains/Toolchain-x86_64-w64-mingw32.cmake
@@ -0,0 +1,39 @@
+# MexIFace CMake build system
+# Mark J. Olah (mjo@cs.unm DOT edu)
+# 03-2014
+#
+# Change MXE_ROOT to the cross-dev-environment directory.
+# 
+# We need to use shared libraries because a matlab mex module is a shared library.
+# we can use static libraries if the files are compiled with -fPIC.  Thus we
+# are using the MXE shared target
+#
+set(MXE_ROOT $ENV{MEXIFACE_MXE_ROOT})
+set(MXE_TARGET_ARCH x86_64-w64-mingw32.shared)
+set(MXE_HOST_ARCH x86_64-unknown-linux-gnu)
+set(MXE_ARCH_ROOT ${MXE_ROOT}/usr/${MXE_TARGET_ARCH})
+set(MXE_BIN_DIR ${MXE_ROOT}/usr/bin)
+# set(CMAKE_MODULES_DIR ${MXE_ROOT}/usr/${MXE_HOST_ARCH}/share/cmake-3.7/Modules)
+
+message(STATUS "[MexIFace] MXE_ROOT: ${MXE_ROOT}")
+#Look here for libraries at install time
+# set(LIB_SEARCH_PAT-HS "${MXE_ROOT}/usr/${TARGET_MXE_ARCH}/lib"
+#                      "${MXE_ROOT}/usr/${TARGET_MXE_ARCH}/bin"
+#                      "${MXE_ROOT}/usr/bin"
+#                      "${MXE_ROOT}/usr/lib"
+#                      "${MXE_ROOT}/usr/lib/gcc/x86_64-w64-mingw32.shared/4.9.4/"
+#                      "${USER_W64_CROSS_ROOT}/lib")
+
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_SYSTEM_PROGRAM_PATH ${MXE_BIN_DIR})
+set(CMAKE_C_COMPILER ${MXE_TARGET_ARCH}-gcc)
+set(CMAKE_CXX_COMPILER ${MXE_TARGET_ARCH}-g++)
+set(CMAKE_RC_COMPILER ${MXE_TARGET_ARCH}-windres)
+
+set(CMAKE_FIND_ROOT_PATH ${MXE_ROOT} ${MXE_ARCH_ROOT})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
