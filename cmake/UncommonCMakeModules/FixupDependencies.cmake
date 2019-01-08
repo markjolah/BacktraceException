0385f2915752a335cc55da128c8c947846c6f2e5
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-08 10:29:52 -0700

    git subrepo pull cmake/UncommonCMakeModules
    subrepo:
      subdir:   "cmake/UncommonCMakeModules"
      merged:   "08acafd"
    upstream:
      origin:   "https://github.com/markjolah/UncommonCMakeModules.git"
      branch:   "master"
      commit:   "08acafd"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

--- cmake/UncommonCMakeModules/FixupDependencies.cmake
+++ cmake/UncommonCMakeModules/FixupDependencies.cmake
@@ -7,29 +7,35 @@
 # See: LICENCE file
 #
 # Single argument keywords:
-#   COPY_DESTINATION - [optional] [default: '.'] Relative path from the install location to the copy location
+#   COPY_RPATH - [optional] [default: '.'] Relative path from the target install location to the lib dir  i.e., copy location.
 #   TARGET_DESTINATION - [suggested but optional] [default try to find_file in install prefix].  The relative path
 #       of the target under the install prefix.  This is the same value as given to DESTINATION keyword of install(TARGETS).
+#   FIXUP_SCRIPT_TEMPLATE - [optional] [default: look for script in ../Templates or ./Templates]
 # Multi-argument keywords:
 #   TARGETS - List of targets to copy dependencies for.  These targets should share all of the other keyword propoerties.
 #             If multiple targets require different options to fixup_dependencies, then multiple independent calls should be made.
 #   LIB_SEARCH_PATHS - Search path for libraries that should be copied into the install tree.
 #   LIB_SYSTEM_PATHS - Search paths for system libraries that should be assumed to be provided by the client system and should not be copied.
-function(fixup_dependencies target)
-    if(NOT TARGET ${target})
-        message(FATAL_ERROR "fixup_dependencies works on cmake targets.  Got target=${target}")
-    endif()
-    cmake_parse_arguments(FIXUP "" "COPY_DESTINATION;TARGET_DESTINATION" "TARGETS;LIB_SEARCH_PATHS;LIB_SYSTEM_PATHS" ${ARGN})
-    if(NOT FIXUP_COPY_DESTINATION)
-        set(FIXUP_COPY_DESTINATION ".")  #Must be relative to TARGET_DESTINATION
+set(_fixup_dependencies_install_PATH ${CMAKE_CURRENT_LIST_DIR})
+function(fixup_dependencies)
+    cmake_parse_arguments(FIXUP "" "COPY_RPATH;TARGET_DESTINATION;SCRIPT_TEMPLATE" "TARGETS;LIB_SEARCH_PATHS;LIB_SYSTEM_PATHS" ${ARGN})
+    if(NOT FIXUP_COPY_RPATH)
+        set(FIXUP_COPY_RPATH ".")  #Must be relative to TARGET_DESTINATION
     endif()
     if(NOT FIXUP_TARGET_DESTINATION)
         set(FIXUP_TARGET_DESTINATION) #Signal to use find_file in FixupTarget script
     endif()
+    if(NOT FIXUP_SCRIPT_TEMPLATE)
+        find_file(FIXUP_SCRIPT_TEMPLATE_PATH FixupTargetDependenciesScript.cmake.in
+                PATHS ${_fixup_dependencies_install_PATH}/Templates ${_fixup_dependencies_install_PATH}/../Templates
+                NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
+        mark_as_advanced(FIXUP_SCRIPT_TEMPLATE_PATH)
+        if(NOT FIXUP_SCRIPT_TEMPLATE_PATH)
+            message(FATAL_ERROR "[fixup_dependencies]: Cannot find FixupTargetDependenciesScript.cmake.in")
+        endif()
+        set(FIXUP_SCRIPT_TEMPLATE ${FIXUP_SCRIPT_TEMPLATE_PATH})
+    endif()
 
-#     set(FIXUP_RPATH "." ${FIXUP_COPY_DESTINATION} ${FIXUP_RPATH})
-#     list(REMOVE_DUPLICATES FIXUP_RPATH)
-    set(FIXUP_SCRIPT ${CMAKE_BINARY_DIR}/Fixup-${target}.cmake)
     #Try to eliminate the need for this block
     if(WIN32)
         set(TARGET_OS WIN64)
@@ -38,8 +44,16 @@ function(fixup_dependencies target)
     else()
         set(TARGET_OS OSX)
     endif()
-    
-    configure_file(${MexIFace_CMAKE_TEMPLATES_DIR}/FixupTarget.cmake.in ${FIXUP_SCRIPT}.gen @ONLY)
-    file(GENERATE OUTPUT ${FIXUP_SCRIPT} INPUT ${FIXUP_SCRIPT}.gen )
-    install(SCRIPT ${FIXUP_SCRIPT})
+    foreach(FIXUP_TARGET IN LISTS FIXUP_TARGETS)
+        message(STATUS "FIXUP_TARGET:${FIXUP_TARGET}")
+        if(NOT TARGET ${FIXUP_TARGET})
+            message(FATAL_ERROR "[fixup_dependencies]  Got invalid target:${FIXUP_TARGET}")
+        endif()
+        set(FIXUP_SCRIPT ${CMAKE_BINARY_DIR}/Fixup-${FIXUP_TARGET}.cmake)
+
+        configure_file(${FIXUP_SCRIPT_TEMPLATE} ${FIXUP_SCRIPT}.gen @ONLY)
+        file(GENERATE OUTPUT ${FIXUP_SCRIPT} INPUT ${FIXUP_SCRIPT}.gen )
+        install(SCRIPT ${FIXUP_SCRIPT})
+        message(STATUS "[fixup_dependencies]  Generated dependency fixup script for target: ${FIXUP_TARGET}")
+    endforeach()
 endfunction()
