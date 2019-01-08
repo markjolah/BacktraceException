701305ebca749138485dd02831ab502c94513181
Mark J. Olah <markjolah@users.noreply.github.com>
2019-01-08 11:58:53 -0700

    Working on FixupDependencies

--- cmake/UncommonCMakeModules/Toolchains/Toolchain-MXE-x86_64-w64-mingw32.cmake
+++ cmake/UncommonCMakeModules/Toolchains/Toolchain-MXE-x86_64-w64-mingw32.cmake
@@ -1,27 +1,21 @@
 # MexIFace CMake build system
 # Mark J. Olah (mjo@cs.unm DOT edu)
-# 03-2014
+# 2014-2018
 #
-# Change MXE_ROOT to the cross-dev-environment directory.
+# This toolchain uses the MXE crossdev environment
+# set MXE_ROOT to the cross-dev-environment directory.
 # 
-# We need to use shared libraries because a matlab mex module is a shared library.
-# we can use static libraries if the files are compiled with -fPIC.  Thus we
-# are using the MXE shared target
 #
-set(MXE_ROOT $ENV{MEXIFACE_MXE_ROOT})
+set(MXE_ROOT "$ENV{MXE_ROOT}")
 set(MXE_TARGET_ARCH x86_64-w64-mingw32.shared)
 set(MXE_HOST_ARCH x86_64-unknown-linux-gnu)
 set(MXE_ARCH_ROOT ${MXE_ROOT}/usr/${MXE_TARGET_ARCH})
 set(MXE_BIN_DIR ${MXE_ROOT}/usr/bin)
 
 message(STATUS "[MexIFace] MXE_ROOT: ${MXE_ROOT}")
-#Look here for libraries at install time
-# set(LIB_SEARCH_PAT-HS "${MXE_ROOT}/usr/${TARGET_MXE_ARCH}/lib"
-#                      "${MXE_ROOT}/usr/${TARGET_MXE_ARCH}/bin"
-#                      "${MXE_ROOT}/usr/bin"
-#                      "${MXE_ROOT}/usr/lib"
-#                      "${MXE_ROOT}/usr/lib/gcc/x86_64-w64-mingw32.shared/4.9.4/"
-#                      "${USER_W64_CROSS_ROOT}/lib")
+
+#Search paths for dependet libraries to copy into install dir
+set(FIXUP_LIB_SEARCH_PATHS "${MXE_ROOT}/usr/${MXE_TARGET_ARCH}/bin")
 
 set(CMAKE_SYSTEM_NAME Windows)
 
