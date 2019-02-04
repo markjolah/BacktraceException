3c08a8386c7d210e7c6ec23ea101736dc685bb5b
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 13:27:12 -0700

    build script updates

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# build.sh
+#
+# Simple release build
 
 INSTALL_PATH=_install
-BUILD_PATH=_build
+BUILD_PATH=_build/Release
 NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-COMMON_ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DBUILD_SHARED_LIBS=ON -DBUILD_STATIC_LIBS=ON"
+
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DOPT_INSTALL_TESTING=Off"
+ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=Off"
+
+set -ex
 rm -rf $INSTALL_PATH $BUILD_PATH
-
-set -e
-
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${COMMON_ARGS}
-cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${COMMON_ARGS}
-cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
-cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS}
+cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
