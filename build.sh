d8c877be2e9bb60236e7d9581529ad9508bd97cc
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 18:39:57 -0700

    up

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ if [ "$INSTALL_PATH" == "_install" ]; then
 fi
 rm -rf $BUILD_PATH/Debug
 rm -rf $BUILD_PATH/Release
-cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS}
+cmake -H. -B$BUILD_PATH/Debug -DCMAKE_BUILD_TYPE=Debug ${ARGS}
 cmake --build $BUILD_PATH/Debug --target install -- -j${NUM_PROCS}
 cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS}
 cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
