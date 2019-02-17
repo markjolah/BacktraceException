df972f75641d495d045e9c8af5b9cb3e18edf5e8
Docu-Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-17 22:18:10 +0000

    Deploy markjolah/BacktraceException to github.com/markjolah/BacktraceException.git:gh-pages

---
+++ search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "_bcdegprsw",
+  1: "b",
+  2: "b",
+  3: "br",
+  4: "bcdegpsw",
+  5: "_",
+  6: "b",
+  7: "gs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "enums",
+  7: "enumvalues"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Enumerations",
+  7: "Enumerator"
+};
+
