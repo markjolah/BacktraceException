7c76e21d2c9aebeb2896f1a6106653b5735b7f0b
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-26 12:14:34 -0700

    Build system update

--- build.debug.sh
+++ build.debug.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # build.debug.sh <cmake args ...>
+#
+# BacktraceException example debug build script.
+#
 # Debug-only build to local install prefix with build-tree export
 
 INSTALL_PATH=_install
 BUILD_PATH=_build/Debug
-NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
-
+NUM_PROCS=$(grep -c ^processor /proc/cpuinfo)
 ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
-ARGS="${ARGS} -DBUILD_TESTING=On"
-ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=OFF"
 ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DOPT_DOC=Off"
 ARGS="${ARGS} -DBUILD_TESTING=On"
 ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
 ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
 
 set -ex
-rm -rf $BUILD_PATH
+#rm -rf $BUILD_PATH
 cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS} $@
-VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
+VERBOSE=1 cmake --build $BUILD_PATH --target all -- -j${NUM_PROCS}
+#VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # build.sh <cmake-args...>
 #
-# Release only build to local install prefix with build-tree export and testing.
+# BacktraceException default build script.
 #
-# For safety only delete the _install if and only if INSTALL_PATH hasn't been modified.
+# Clean release-only build to local install prefix with build-tree export support.
+# Cleans up build and install directories.  For safety, deletes the install dir
+# if and only if INSTALL_PATH hasn't been modified from the default "_install"
+
 
 INSTALL_PATH=_install
 BUILD_PATH=_build
@@ -12,6 +15,7 @@ NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
 ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
 ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
 ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DOPT_DOC=Off"
 ARGS="${ARGS} -DBUILD_TESTING=On"
 ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
 ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
@@ -21,6 +25,7 @@ set -ex
 if [ "$INSTALL_PATH" == "_install" ]; then
     rm -rf _install
 fi
+
 rm -rf $BUILD_PATH/Release
-cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS} $@
+cmake -H. -B$BUILD_PATH/Release -DCMAKE_BUILD_TYPE=Release ${ARGS}
 cmake --build $BUILD_PATH/Release --target install -- -j${NUM_PROCS}
